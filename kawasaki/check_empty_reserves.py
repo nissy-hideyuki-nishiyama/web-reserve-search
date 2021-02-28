d65101773c7b59f12440ee012e8ba89fcf0b1ba7
Nishiyama,Hideyuki <hnishi@floppy-1.com>
2021-02-28 01:29:47 +0000

    delete selenium package

--- kawasaki/check_empty_reserves.py
+++ kawasaki/check_empty_reserves.py
@@ -1,12 +1,3 @@
-# HTMLクローラー関連
-from selenium import webdriver
-#from selenium.webdriver.common.by import By
-#from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.common import exceptions
-from selenium.webdriver.support import expected_conditions as EC
-
 # カレンダー関連
 from time import sleep
 import math
