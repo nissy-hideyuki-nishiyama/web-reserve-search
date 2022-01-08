f2741dfd19c9a6fa9b5d75573e393cda7508937f
Nishiyama,Hideyuki <hnishi@floppy-1.com>
2022-01-08 09:11:02 +0000

    modified: fix bugs.

--- machida/check_empty_reserves_multi_threads.py
+++ machida/check_empty_reserves_multi_threads.py
@@ -57,26 +57,38 @@ from reserve_tools import reserve_tools
 
 http_req_num = 0
 
-## 空き予約時間帯を検索する空き予約リストへの登録を排他制御する
-class AsyncioLockReservesList:
-    """
-    スレッド処理に対応するため、排他制御で空きコート時間リストの登録を行う
-    """
-    lock = asyncio.Lock()
+# スレッドセーフな空き予約リスト
+class ThreadSafeReservesList:
+    lock = threading.Lock()
     def __init__(self):
         self.reserves_list = {}
-    async def add_url(self, day, time, court_name):
-        async with self.lock:
-            await self.reserves_list[f'{day}'].setdefault(time, []).append(court_name)
+    def add_reserves(self, _date, _time, _locate_court, logger=None):
+        with self.lock:
+            # 空き予約リストに追加する
+            # 空き予約リストに発見した日がなければ、年月日をキーとして初期化する
+            if _date not in self.reserves_list:
+                self.reserves_list[_date] = {}
+                self.reserves_list[_date].setdefault(_time, []).append(_locate_court)
+            # 空き予約リストに発見した日が登録されていた場合
+            else:
+                # 空き予約リストに発見した時間帯がなければ、時間をキーとしてリストを初期化する
+                if _time not in self.reserves_list[_date]:
+                    self.reserves_list[_date][_time] = []
+                self.reserves_list[_date][_time].append(_locate_court)
+            #logger.debug(f'{self.reserves_list}')
 
 # クローラー
 ## Selenium関連
-def setup_driver(headers):
+def setup_driver():
     """
     seleniumを初期化する
     デバッグ時にはChromeの動作を目視確認するために、options.add_argument行をコメントアウトする。
     ヘッドレスモードを利用する場合は、options.add_argument行のコメントアウトを外す。
     """
+    # WEB request header
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36'
+    }
     # Chromeを指定する
     chromedriver_location = '/usr/lib64/chromium-browser/chromedriver'
     chrome_service = fs.Service(executable_path=chromedriver_location)
@@ -106,13 +118,14 @@ def selenium_get_cookie_and_html(driver, cfg, logger=None):
     response = driver.get(cfg['top_url'])
     http_req_num += 1
     cookies = driver.get_cookies()
-    logger.debug(f'Top cookies:')    
-    logger.debug((json.dumps(cookies, indent=2)))
+    # logger.debug(f'Top cookies:')
+    # logger.debug((json.dumps(cookies, indent=2)))
     sleep(1)
+    # メニューページにアクセスする
     response = driver.get(cfg['first_url'])
     cookies = driver.get_cookies()
-    logger.debug(f'First URL cookies:')    
-    logger.debug((json.dumps(cookies, indent=2)))
+    # logger.debug(f'First URL cookies:')
+    # logger.debug((json.dumps(cookies, indent=2)))
     http_req_num += 1
     sleep(1)
     #cookies = driver.get_cookies()
@@ -120,7 +133,7 @@ def selenium_get_cookie_and_html(driver, cfg, logger=None):
     _html = driver.page_source
     with open('dselect.html', mode='w', encoding='utf-8', errors='ignore') as f:
         f.write(_html)
-    # 空き検索ページにアクセスする
+    # 空き予約検索ページにアクセスする
     response = driver.get(cfg['second_url'])
     http_req_num += 1
     cookies = driver.get_cookies()
@@ -128,9 +141,8 @@ def selenium_get_cookie_and_html(driver, cfg, logger=None):
     _html = driver.page_source
     with open('Wp_TopMenu.html', mode='w', encoding='utf-8', errors='ignore') as f:
         f.write(_html)
-    logger.debug(f'Wp_TopMenu cookies:')
-    #logger.debug(f'{cookies}')
-    logger.debug((json.dumps(cookies, indent=2)))
+    # logger.debug(f'Wp_TopMenu cookies:')
+    # logger.debug((json.dumps(cookies, indent=2)))
     # 「空き照会」ボタンが表示されるまで待機する
     #elment = wait.until(EC.presence_of_element_located((By.XPATH, "//*[@id="wpManager_gwppnlLeftZone_btnShoukai"]")))
     elment = wait.until(EC.presence_of_all_elements_located)
@@ -140,9 +152,9 @@ def selenium_get_cookie_and_html(driver, cfg, logger=None):
 
 # トップページから空き予約検索ページに接続し、cookieとフォームデータのためのHTMLファイルを取得する
 @reserve_tools.elapsed_time
-def connect_to_get_cookies_and_html(cfg, logger=None):
+def connect_to_top_and_menu(cfg, logger=None):
     """
-    トップページに接続し、cookieを取得する
+    トップページから空き予約検索ページに接続し、cookieとフォームデータのためのHTMLファイルを取得する
     JavaScript未対応のブラウザの場合はワーニングページに飛ばされるため、WEBブラウザを使う
     Args:
         cfg ([type]): [description]
@@ -155,9 +167,8 @@ def connect_to_get_cookies_and_html(cfg, logger=None):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
     } 
     # クローラーの初期化
-    ( driver, mouse ) = setup_driver(headers)
-    # 空き予約ページにアクセスし、cookieとフォームデータを取得する
-    #( cookies , html )= selenium_get_cookie_and_html(driver, cfg, logger=logger)
+    ( driver, mouse ) = setup_driver()
+    # トップページから空き予約検索ページに接続し、cookieとフォームデータのためのHTMLファイルを取得する
     ( driver )= selenium_get_cookie_and_html(driver, cfg, logger=logger)
     return driver
 
@@ -168,7 +179,7 @@ def selenium_post_conditions(driver, date_list, reserves_list, cfg, logger=None)
     空き予約情報を取得する
     """
     # 待機時間を設定する
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 10, 2)
     # 検索ページがすべて表示されるまで待機する
     wait.until(EC.presence_of_all_elements_located)
     #sleep(5)
@@ -207,7 +218,7 @@ def selenium_input_datas(driver, input_date, logger=None):
     Time = '全日'
     # DOM上に表示されるまで待機する
     # 検索フォームのフィールド設定
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 10, 2)
     # 施設の分類
     f_SSDaiClass = wait.until(EC.presence_of_element_located((By.ID, "wpManager_gwppnlLeftZone_cmbSSDaiClass")))
     Select(f_SSDaiClass).select_by_value(f'{SSDaiClass}')
@@ -227,14 +238,6 @@ def selenium_input_datas(driver, input_date, logger=None):
     f_Time = wait.until(EC.presence_of_element_located((By.ID, "wpManager_gwppnlLeftZone_ucTermSettings_cmbTime")))
     Select(f_Term).select_by_value(f'{Time}')
     f_shisetsu = wait.until(EC.presence_of_element_located((By.ID, "shisetsu")))
-    # 分類フィールドで施設を選択する
-    Select(f_shisetsu).select_by_index(shisetsuId)
-    # 開始日フィールドが表示されるまで待機後、指定する
-    f_date = wait.until(EC.presence_of_element_located((By.NAME, "date")))
-    # 開始日フィールドに入力されている値をクリアする
-    f_date.clear()
-    # 開始日フィールドに指定日を入力する
-    f_date.send_keys(str(input_date))
     # 画面を最下行までスクロールさせ、全ページを表示する
     driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
     # 「空き照会>>」ボタンをクリックする
@@ -298,7 +301,7 @@ def get_empty_court_time(cfg, threadsafe_list, date, html, logger=None):
     return None
 
 # メニューボタンで空き予約検索ページに戻る
-def go_to_search_reserves_page(driver, logger=logger):
+def go_to_search_reserves_page(driver, logger=None):
     """[summary]
 
     Args:
@@ -309,7 +312,7 @@ def go_to_search_reserves_page(driver, logger=logger):
         [type]: [description]
     """
     # 待機時間を設定する
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 10, 2)
     # 検索ページがすべて表示されるまで待機する
     wait.until(EC.presence_of_all_elements_located)
     # 「メニューへ」ボタンをクリックする
@@ -319,26 +322,22 @@ def go_to_search_reserves_page(driver, logger=logger):
     sleep(1)
     # 「クリアー」ボタンを押して、検索条件をクリアーする
     f_ClearBtn = wait.until(EC.element_to_be_clickable((By.ID, "wpManager_gwppnlLeftZone_btnClear")))
-    f_clearBtn.click()
+    f_ClearBtn.click()
     # 
     return driver
 
 # webdriver初期化から空き予約検索、webdriver終了の一連の動作をする
 @reserve_tools.elapsed_time
-def date_search(cfg, headers, date_list, threadsafe_list, logger=None):
+def date_search(cfg, date_list, threadsafe_list, logger=None):
     """
-    webdriverを初期化する
-    cookieを取得する
+    トップページ、メニューページに接続し、検索に必要なcookieなどを取得する
     空き情報を検索するページに移動する
     条件を指定して空き予約を取得する
     排他制御で空き予約リストにレコードを登録する
     webdriverを終了する
     """
-    # クローラーの初期化
-    ( driver, mouse ) = setup_driver(headers)
-    # トップページから空き予約検索ページに移動し、cookieなどを取得する
-    #( cookies , response )= selenium_get_cookie(driver, cfg, logger=logger)
-    ( driver, shtml )= connect_to_get_cookies_and_html(driver, cfg, logger=logger)
+    # トップページ、メニューページに接続し、検索に必要なcookieなどを取得する
+    ( driver )= connect_to_top_and_menu(cfg, logger=logger)
     # 条件を指定して、空き予約を検索し、空き予約を取得する
     threadsafe_list = selenium_post_conditions(driver, date_list, threadsafe_list, cfg, logger=logger)
     driver.quit()
@@ -346,7 +345,7 @@ def date_search(cfg, headers, date_list, threadsafe_list, logger=None):
 
 # マルチスレッドで、空き予約を検索する
 @reserve_tools.elapsed_time
-def multi_thread_datesearch(cfg, headers, date_list_threads, threadsafe_list, threads_num=1, logger=None):
+def multi_thread_datesearch(cfg, date_list_threads, threadsafe_list, threads_num=1, logger=None):
     """
     マルチスレッドで次のことを実施する
     webdriverを初期化する
@@ -363,7 +362,7 @@ def multi_thread_datesearch(cfg, headers, date_list_threads, threadsafe_list, th
         #_index = 0
         for date_list in date_list_threads:
             # スレッド数に応じて分割した検索対象年月日リストを受け取って、検索する
-            future = executor.submit(date_search, cfg, headers, date_list, threadsafe_list, logger=logger)
+            future = executor.submit(date_search, cfg, date_list, threadsafe_list, logger=logger)
             futures.append(future)
         for future in as_completed(futures):
             future.result()
@@ -401,28 +400,34 @@ def prepare_serch_empty_reserves(cfg_filename="cfg.json"):
 
 # 空き予約の検索処理のメイン
 @reserve_tools.elapsed_time
-def main_search_empty_reserves(cfg, date_list, coro, limit=4, logger=None):
+def main_search_empty_reserves(cfg, date_list, threads_num=1, logger=None):
     """
     初期化処理
     """
     # 初期化処理(主に非同期並列処理のための変数の作成)
     # HTTPリクエスト数
     global http_req_num
+    # 空き予約リストの初期化
+    threadsafe_list = ThreadSafeReservesList()
     # 実行時間を測定する
     start = time.time()
     # 検索対象日をスレッド数に応じたリストに分割する
     split_date_lists = reserve_tools.split_date_list_by_threads(cfg, date_list, logger=logger)
     #request_objs = create_request_objs(cfg, date_list, cookies, form_datas, logger=logger)
     #return None
-    ( cookies, shtml )= connect_to_get_cookies_and_html(cfg, logger=logger)
+    #( cookies, shtml )= connect_to_get_cookies_and_html(cfg, logger=logger)
     # フォームデータを作成する
     #( form_data ) = create_form_data(cfg, shtml, logger=logger)
     # 空き予約検索を実行する
-    ( reserves ) =  get_empty_reserves_html_by_chrome(cfg, cookies, form_data, logger=logger)
+    #( reserves ) =  get_empty_reserves_html_by_chrome(cfg, cookies, form_data, logger=logger)
     
     # マルチスレッドで呼び出す
-    threadsafe_list = multi_thread_datesearch(cfg, headers, date_list_threads, threadsafe_list, threads_num=threads_num, logger=logger)
+    threadsafe_list = multi_thread_datesearch(cfg, split_date_lists, threadsafe_list, threads_num=threads_num, logger=logger)
     logger.debug(json.dumps(threadsafe_list.reserves_list, indent=2, ensure_ascii=False))
+    
+    # 実行時間を表示する
+    elapsed_time = time.time() - start
+    logger.info(f'main_search_empty_reserves() duration time: {elapsed_time} sec')
     
     return None
 
@@ -440,48 +445,15 @@ def postproc_search_empty_reserves():
 if __name__ == '__main__':
     # 実行時間を測定する
     _start = time.time()
-
-    # 非同期ロック
-    async_lock_reserves = AsyncioLockReservesList()
-    reserves_list = async_lock_reserves.reserves_list
     # 事前準備
     ( cfg, logger, date_list ) = prepare_serch_empty_reserves(cfg_filename="cfg.json")
     logger.info(f'starting to search empty reserve.')
     # 同時実行数
     threads = cfg['threads_num']
-    #( cookies, form_datas ) = get_cookie(cfg)
-    # 空き予約検索データを作成する
-    #request_objs = create_request_objs(cfg, date_list, cookies, form_datas, logger=logger)
     # 検索対象年月日を指定して、空き年月日のHTMLボディを取得する
-    #results = main(request_objs, coro=coroutine, limit=threads, logger=logger)
-    results = main_search_empty_reserves(cfg, date_list, coro=coroutine, limit=threads, logger=logger)
-    # 空きコートのリンクを作成する
-    # for url, status, body in results:
-    #     get_empty_court(body, cfg, url[0], court_link_list, logger=logger)
-    # logger.debug(f'')
-    # logger.debug(f'#### Analyzed Link End : ####')
-    # logger.debug(f'')
-    #print(json.dumps(court_link_list, indent=2))
-    # 実行時間を表示する
-    # elapsed_time = time.time() - _start
-    # logger.debug(f'search empty court link duration time: {elapsed_time} sec')
-    #print(json.dumps(reserves_list, indent=2, ensure_ascii=False))
-    # 空き予約リストを昇順にソートする
-    # sorted_reserves_list = reserve_tools.sort_reserves_list(reserves_list)
-    # # LINEにメッセージを送信する
-    # postproc(reserves_list, logger=logger)
-    # logger.info(f'finished to search empty reserve.')
-    # # 空き予約リストに値があるかないかを判断し、予約処理を開始する
-    # #print(f'reserves_list: {threadsafe_list.reserves_list}')
-    # if len(reserves_list) == 0:
-    #     logger.info(f'stop do reserve because no empty reserve.')
-    # else:
-    #     logger.info(f'starting reserve process.')
-    #     reserve_result = main3(cfg, sorted_reserves_list, want_date_list, logger=logger)
-    #     #return None
+    results = main_search_empty_reserves(cfg, date_list, threads_num=threads, logger=logger)
     # 空き検索処理の事後処理
     #postproc_search_empty_reserves(logger=logger)
-    
     # デバッグ用(HTTPリクエスト回数を表示する)
     logger.info(f'HTTP リクエスト数 whole(): {http_req_num} 回数')
     # 実行時間を表示する
