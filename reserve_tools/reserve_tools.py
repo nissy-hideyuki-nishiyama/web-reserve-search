7681bd8977a43720dc924524b0bcffa0b2ee70ed
Nishiyama,Hideyuki <hnishi@floppy-1.com>
2022-12-24 03:06:46 +0000

    modified: fix bugs of create_want_day_list.

--- reserve_tools/reserve_tools.py
+++ reserve_tools/reserve_tools.py
@@ -301,15 +301,12 @@ def create_want_day_list(month, public_holiday, cfg):
     after_month = after_date.month
     after_day = after_date.day
     # 月の処理
-    # X日後の月が指定月より大きい場合
-    if month < after_month:
-        return day_list
-    # X日後の月が指定月と同じ場合
-    elif month == after_month:
+    # X日後の月が指定月と同じ場合は、今日からX日後の日付(after_day)を_ref_dayとする
+    if month == after_month:
         _ref_day = after_day
-    # X日後の月が指定月より小さい場合
+    # X日後の月が翌月の場合は、対象月は空の日付リストを返す
     else:
-        _ref_day = 0
+        return day_list
     # 選択された曜日の日にちのリストを作成する
     for _wday in selected_weekdays:
         _day = _wday - first_weekday + 1
@@ -339,7 +336,7 @@ def create_want_day_list(month, public_holiday, cfg):
             # 予約希望除外日リストが存在した場合、削除する
             if _exclude_day in target_days:
                 target_days.remove(int(_exclude_day))
-    #print(target_days)
+    print(target_days)
     return target_days
 
 # 予約希望日リストを作成する
